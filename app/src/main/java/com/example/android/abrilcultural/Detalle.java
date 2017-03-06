d70c4bc1b85839b7fb1c365941f2f33657c50b5c
PC <florez190386@gmail.com>
2017-03-06 13:03:30 -0300

    version interfaz terminada

--- app/src/main/java/com/example/android/abrilcultural/Detalle.java
+++ app/src/main/java/com/example/android/abrilcultural/Detalle.java
@@ -1,13 +1,23 @@
 package com.example.android.abrilcultural;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class Detalle extends AppCompatActivity {
-
+  private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Cultura salta ");
+        //toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+
     }
+
+
 }
