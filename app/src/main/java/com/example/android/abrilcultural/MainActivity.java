159f290e14331804c08f682bb8d44db2266126c5
florez857 <florez190386@gmail.com>
2017-03-08 19:01:07 -0300

    completo3

--- app/src/main/java/com/example/android/abrilcultural/MainActivity.java
+++ app/src/main/java/com/example/android/abrilcultural/MainActivity.java
@@ -113,7 +113,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
